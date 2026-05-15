bc49187f2c1590945e8808283b9bdd94bbb30421
EfrainHernandezConcha <efrain_concha2@hotmail.com>
2026-05-15 12:19:58 -0600

    Log dimensions for containerImg2 on load and resize
    Added logging for dimensions of containerImg2.

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-        // Optional: Log container dimensions
+// ContainerImg1
         const container = document.querySelector('.container-img');
         
         function logContainerDimensions() {
             console.log('Container Width:', container.offsetWidth + 'px');
             console.log('Container Height:', container.offsetHeight + 'px');
+        }
+
+        // Log on load
+        window.addEventListener('load', logContainerDimensions);
+
+        // Log on resize
+        window.addEventListener('resize', logContainerDimensions);
+
+
+// ContainerImg2 
+        const containerImg2 = document.querySelector('.container-img2');
+        
+        function logContainerDimensions() {
+            console.log('Container Width:', containerImg2.offsetWidth + 'px');
+            console.log('Container Height:', containerImg2.offsetHeight + 'px');
         }
 
         // Log on load

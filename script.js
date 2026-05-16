73a375d5d85bf0b0be003f8ea4d801871ab2e698
EfrainHernandezConcha <efrain_concha2@hotmail.com>
2026-05-16 10:53:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,22 @@
         // Log on resize
         window.addEventListener('resize', logContainerDimensions);
 
+//funcion zoom on scroll
+const zoom = document.querySelectorAll('.zoom');
+function zoomScroll() {
+  const alturaPantalla = window.innerHeight;
+
+  zoom.forEach(el => {
+    const distancia = el.getBoundingClientRect().top;
+
+    if (distancia < alturaPantalla - 100) {
+      el.classList.add('zoom-on');
+    }
+    else {
+        el.classList.remove('zoom-on')
+      }
+  });
+}
+
+
+window.addEventListener('scroll', zoomScroll);
